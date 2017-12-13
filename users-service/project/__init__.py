993a917fe024e78f05f2743c0c62bee17eac1aa2
Pedro Vanzella <pedro@pedrovanzella.com>
2017-12-13 17:43:52 -0200

    Refactor a bit further

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,10 +1,8 @@
-import os
 from flask import Flask, jsonify
 
 
-def create_app(script_info=None):
+def create_app(app_settings=[]):
     app = Flask(__name__)
-    app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
     return app
 
