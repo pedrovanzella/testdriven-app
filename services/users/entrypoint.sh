a981ea131d99768c232be8c51a37e80c98312ba4
Pedro Vanzella <pedro@pedrovanzella.com>
2018-06-03 16:25:16 -0300

    fix port issues

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -2,9 +2,9 @@
 
 echo "Waiting for postgres..."
 
-while ! nc -z users-db 5433; do
+while ! nc -z users-db 5432; do
     sleep 0.1
 done
 
 echo "PostgreSQL started"
-python manage.py runserver -h 0.0.0.0
+python manage.py run -h 0.0.0.0
